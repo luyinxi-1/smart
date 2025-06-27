de7526aa7690b7fdbe6f853726ce1aa84dc2e318
buyunhao <2689834083@qq.com>
2025-06-27 17:23:44 +0800

    系统管理部分建设

--- base/src/main/java/com/upc/modular/auth/controller/SysDictItemController.java
+++ base/src/main/java/com/upc/modular/auth/controller/SysDictItemController.java
@@ -1,6 +1,7 @@
 package com.upc.modular.auth.controller;
 
 
+import io.swagger.annotations.Api;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/sys-dict-item")
+@Api(tags = "字典项")
 public class SysDictItemController {
 
 }
--- base/src/main/java/com/upc/modular/auth/controller/UserRoleListController.java
+++ base/src/main/java/com/upc/modular/auth/controller/UserRoleListController.java
@@ -1,6 +1,7 @@
 package com.upc.modular.auth.controller;
 
 
+import io.swagger.annotations.Api;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/user-role-list")
+@Api(tags = "用户角色关联")
 public class UserRoleListController {
 
 }
