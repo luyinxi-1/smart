6956d868e9c6e760466a8fd90bc95bd246b2cf07
fanweixu <19819796001@163.com>
2025-06-30 20:10:29 +0800

    学生表的增删改查

--- base/src/main/java/com/upc/modular/auth/controller/param/SysDictTypeParam/IdParam.java
+++ base/src/main/java/com/upc/modular/auth/controller/param/SysDictTypeParam/IdParam.java
@@ -4,10 +4,13 @@ import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Data
 public class IdParam {
     @ApiModelProperty(value = "删除ID列表")
     private List<Long> idList;
+
+
 }
