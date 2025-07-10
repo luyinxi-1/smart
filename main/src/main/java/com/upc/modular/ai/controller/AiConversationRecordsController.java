508fe36a963497ab9d16f45d9e22d4907e42a78b
buyunhao <2689834083@qq.com>
2025-07-10 10:01:46 +0800

    ai对话记录表

--- main/src/main/java/com/upc/modular/ai/controller/AiConversationRecordsController.java
+++ main/src/main/java/com/upc/modular/ai/controller/AiConversationRecordsController.java
@@ -1,10 +1,17 @@
 package com.upc.modular.ai.controller;
 
 
+import com.upc.common.responseparam.R;
+import com.upc.modular.ai.entity.AiConversationRecords;
+import com.upc.modular.ai.param.AIConRecordsSessionId;
+import com.upc.modular.ai.service.IAiConversationRecordsService;
 import io.swagger.annotations.Api;
-import org.springframework.web.bind.annotation.RequestMapping;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 /**
  * <p>
@@ -18,5 +25,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/ai-conversation-records")
 @Api(tags = "ai对话记录")
 public class AiConversationRecordsController {
+    @Autowired
+    private IAiConversationRecordsService aiConversationRecordsService;
+    @PostMapping("/selectDeepseekConRecords")
+    @ApiOperation("查询DeepseekConRecords")
+    public R<List<AiConversationRecords>> selectDeepseekConRecords(@RequestParam Long dpConId) {
+        List<AiConversationRecords> returnList = aiConversationRecordsService.selectDeepseekConRecords(dpConId);
 
+        if (returnList == null || returnList.isEmpty()) {
+            return R.fail("未找到相关记录");
+        }
+
+        return R.ok(returnList);
+    }
+
+    @PostMapping("/insertDeepseekConRecords")
+    @ApiOperation("新增DeepseekConRecords")
+    @Transactional
+    public R<Long> insertDeepseekConRecords(@RequestBody AIConRecordsSessionId DeepseekConRecordsSessionId) {
+        Long dpConId = aiConversationRecordsService.insertDeepseekConRecords(DeepseekConRecordsSessionId);
+        return R.ok(dpConId);
+    }
 }
--- main/src/main/java/com/upc/modular/ai/service/impl/AiConversationRecordsServiceImpl.java
+++ main/src/main/java/com/upc/modular/ai/service/impl/AiConversationRecordsServiceImpl.java
@@ -1,10 +1,18 @@
 package com.upc.modular.ai.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
+import com.upc.modular.ai.entity.AiConversation;
 import com.upc.modular.ai.entity.AiConversationRecords;
 import com.upc.modular.ai.mapper.AiConversationRecordsMapper;
+import com.upc.modular.ai.param.AIConRecordsSessionId;
 import com.upc.modular.ai.service.IAiConversationRecordsService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.upc.modular.ai.service.IAiConversationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * <p>
@@ -16,5 +24,46 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class AiConversationRecordsServiceImpl extends ServiceImpl<AiConversationRecordsMapper, AiConversationRecords> implements IAiConversationRecordsService {
+    @Autowired
+    private IAiConversationService aiConversationService;
+    @Override
+    public List<AiConversationRecords> selectDeepseekConRecords(Long dpConId) {
+        QueryWrapper<AiConversationRecords> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("ai_conversation_id", dpConId)
+                .orderByAsc("add_datetime"); // 按时间从远到近排序
+
+        return this.list(queryWrapper); // 返回列表
+    }
+
+    @Override
+    public Long insertDeepseekConRecords(AIConRecordsSessionId deepseekConRecordsSessionId) {
+        Long dpConId = deepseekConRecordsSessionId.getAiConversationId();
+        System.out.println(deepseekConRecordsSessionId);
+        if (dpConId == null || aiConversationService.getById(dpConId) == null) {
+            Long newMainId = aiConversationService.createConversationIfNeeded(deepseekConRecordsSessionId);
+            dpConId = newMainId; // 赋值新的主表 ID
+        }
+
+        // 插入子表记录
+        AiConversationRecords newdeepseekConRecords = new AiConversationRecords()
+                .setAiConversationId(dpConId)
+                .setContent(deepseekConRecordsSessionId.getContent())
+                .setRole(deepseekConRecordsSessionId.getRole())
+                .setContentThink(deepseekConRecordsSessionId.getContentThink())
+                .setSearch(deepseekConRecordsSessionId.getSearch())
+                .setCreator(deepseekConRecordsSessionId.getCreator())
+                .setAddDatetime(deepseekConRecordsSessionId.getAddDatetime())
+                .setOperator(deepseekConRecordsSessionId.getOperator())
+                .setOperationDatetime(deepseekConRecordsSessionId.getOperationDatetime());
+
+        this.save(newdeepseekConRecords);
+        if (StringUtils.isNotBlank(deepseekConRecordsSessionId.getSessionId())) {
+            AiConversation update = new AiConversation();
+            update.setId(dpConId);
+            update.setConversationId(deepseekConRecordsSessionId.getSessionId());
+            aiConversationService.updateById(update);
+        }
+        return dpConId;
+    }
 
 }
