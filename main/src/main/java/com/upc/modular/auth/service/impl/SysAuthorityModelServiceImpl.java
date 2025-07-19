4bda8f3ab7d714f89e904be3701ef09f3ee36e55
xingth <rosemaryxth@163.com>
2025-07-19 16:21:31 +0800

    权限组管理

---
+++ main/src/main/java/com/upc/modular/auth/service/impl/SysAuthorityModelServiceImpl.java
@@ -0,0 +1,20 @@
+package com.upc.modular.auth.service.impl;
+
+import com.upc.modular.auth.entity.SysAuthorityModel;
+import com.upc.modular.auth.mapper.SysAuthorityModelMapper;
+import com.upc.modular.auth.service.ISysAuthorityModelService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author xth
+ * @since 2025-07-19
+ */
+@Service
+public class SysAuthorityModelServiceImpl extends ServiceImpl<SysAuthorityModelMapper, SysAuthorityModel> implements ISysAuthorityModelService {
+
+}
