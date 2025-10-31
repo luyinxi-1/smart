50162c3ff06816e816289195d2bd9e738842dc32
JM <2531311462@qq.com>
2025-10-31 17:09:08 +0800

    a.通知公告分为系统和教师通知公告，其中系统通知公告是配置的，老师是手动添加的，所以类型需要改成string 类型 【通用】 b.教师通知公告有通知范围的功能 【管理端】 i.教师通知公告有通知范围的功能 ii.教师通知公告有通知范围的功能（传递班级列表）

---
+++ main/src/main/java/com/upc/modular/homepage/mapper/TextbookComprehensiveViewMapper.java
@@ -0,0 +1,29 @@
+package com.upc.modular.homepage.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.upc.modular.homepage.entity.TextbookComprehensiveView;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+/**
+ * <p>
+ * 教材综合视图 Mapper 接口
+ * </p>
+ *
+ * @author byh
+ * @since 2025-10-31
+ */
+@Mapper
+public interface TextbookComprehensiveViewMapper extends BaseMapper<TextbookComprehensiveView> {
+
+    /**
+     * 根据教师用户ID获取收藏教材的学生用户ID列表
+     * @param teacherUserId 教师用户ID
+     * @return 学生用户ID列表
+     */
+    @Select("SELECT DISTINCT student_user_id FROM textbook_comprehensive_view WHERE teacher_user_id = #{teacherUserId}")
+    List<Long> getStudentUserIdsByTeacherUserId(@Param("teacherUserId") Long teacherUserId);
+}
