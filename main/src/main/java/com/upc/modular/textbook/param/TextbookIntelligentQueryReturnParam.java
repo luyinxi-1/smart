a0ac901263bca7db8250b8cf3834a898fff529ac
JM <2531311462@qq.com>
2025-12-05 15:39:18 +0800

    教材智慧搜索

--- main/src/main/java/com/upc/modular/textbook/param/TextbookIntelligentQueryReturnParam.java
+++ main/src/main/java/com/upc/modular/textbook/param/TextbookIntelligentQueryReturnParam.java
@@ -6,6 +6,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -13,6 +15,12 @@ import lombok.NoArgsConstructor;
 public class TextbookIntelligentQueryReturnParam {
     @ApiModelProperty("匹配到的教材名称")
     private String textbookName;
+
+    @ApiModelProperty("匹配到的教材作者")
+    private String authorName;
+
+    @ApiModelProperty("教材更新日期")
+    private LocalDateTime updateDate;
 
     @ApiModelProperty("匹配到的章节名称 (已去除HTML标签)")
     private String chapterName;
--- main/src/main/java/com/upc/modular/textbook/service/ITextbookService.java
+++ main/src/main/java/com/upc/modular/textbook/service/ITextbookService.java
@@ -37,9 +37,9 @@ public interface ITextbookService extends IService<Textbook> {
 
     VersionCheckResultDto checkStatusAndVersion(Long textbookId, String clientVersion);
 
-    TextbookIntelligentQueryReturnParam smartSearch(String query);
-
     Page<TextbookHotnessDto> getTextbookHotnessPage(Page<TextbookHotnessDto> page);
 
     Page<TextbookCenterPageReturnParam> getTextbookCenter(TextbookCenterPageSearchParam param);
+    
+    List<TextbookIntelligentQueryReturnParam> smartSearch(String query);
 }
